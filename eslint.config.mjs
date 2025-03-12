de89c5f9cfe52382f68dad37cffd3d0564992d82
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-12 18:09:40 -0300

    git commit -m "feat(prettier\eslint): configure default formatter and vscode default settings"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      prettier,
+    },
+    rules: {
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
+      "prettier/prettier": ["warn", { endOfLine: "auto" }],
+    },
+  },
+  ...compat.extends("eslint-config-prettier"),
 ];
 
 export default eslintConfig;
